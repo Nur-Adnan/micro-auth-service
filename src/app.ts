34fbb2f9a03fc6066512261861dfdb43fe8aaeb0
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-05-12 11:51:25 +0600

    route, controller started

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,30 @@
 import express, { Request, Response, NextFunction } from "express";
 import logger from "./config/logger";
 import { HttpError } from "http-errors";
+import authRouter from "./routes/auth";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Auth service");
+    res.send("Welcome to Auth service");
 });
+
+app.use("/auth", authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  logger.error(err.message);
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    errors: [
-      {
-        type: err.name,
-        msg: err.message,
-        path: "",
-        location: "",
-      },
-    ],
-  });
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
