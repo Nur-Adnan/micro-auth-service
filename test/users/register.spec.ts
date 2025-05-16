abcd9110cfdd84185ed61589786a81026498c3ce
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-05-16 23:28:05 +0600

    email is missing testing field completed

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -294,5 +294,25 @@ describe("POST /auth/register", () => {
             const users = await userRepository.find();
             expect(users).toHaveLength(0);
         });
+
+        it("shoud return an array of error messages if email is missing", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Nur",
+                lastName: "Adnan",
+                email: "",
+                password: "password",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.body).toHaveProperty("errors");
+            expect(
+                (response.body as Record<string, string>).errors.length,
+            ).toBeGreaterThan(0);
+        });
     });
 });
